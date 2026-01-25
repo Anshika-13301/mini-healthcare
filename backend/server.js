10321bc9f5db126b4aeb23fd58191011e25f4884
Anshika-13301 <shuklaanshika115@gmail.com>
2026-01-25 16:35:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-
 const supportRoutes = require("./Routes/supportRoutes");
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://frontend-7ugq.onrender.com", // frontend link added here
+  methods: ["GET", "POST", "PUT", "DELETE"],    // allowed HTTP methods
+  credentials: true                             // if you need cookies/auth
+}));
+
 app.use(express.json());
 
 // Routes
@@ -18,7 +22,7 @@ app.get("/", (req, res) => {
 });
 
 // Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
 });
